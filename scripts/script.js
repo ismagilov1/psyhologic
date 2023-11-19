ee35b8b6686859d7fa5a703345ac4f3152ac3961
ismagilov1 <ruulllo3@gmail.com>
2023-11-19 22:39:03 +0300

    init

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const myModal = new HystModal({
+  linkAttributeName: "data-hystmodal",
+  // настройки (не обязательно), см. API
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let check = 0;
+
+const openMenu = () => {
+  document.body.style.overflow = "hidden";
+  const burger = document.querySelector(".burger_menu");
+  if (burger.style.display === "flex") {
+    document.body.style.overflow = "scroll";
+    return (burger.style.display = "none");
+  }
+  return (burger.style.display = "flex");
+};
